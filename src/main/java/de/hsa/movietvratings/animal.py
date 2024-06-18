e675997fb4f98a8f5d6db25aafdaa0bb3372504e
Julia Albrecht <julia.albrecht@maibornwolff.de>
2024-06-18 08:03:10 +0200

    add make sound

--- src/main/java/de/hsa/movietvratings/animal.py
+++ src/main/java/de/hsa/movietvratings/animal.py
@@ -3,4 +3,33 @@ class Animal:
             self.name = name
     
     def get_name(self):
+        return self.name
+    
+    def make_sound(self):
         pass
+
+
+class Pigeon(Animal):
+    def make_sound(self):
+        return 'coo'
+
+
+class Lion(Animal):
+    def make_sound(self):
+        return 'roar'
+
+
+class Mouse(Animal):
+    def make_sound(self):
+        return 'squeak'
+
+
+class Snake(Animal):
+    def make_sound(self):
+        return 'hiss'
+
+
+def animal_sound(animals: list):
+    for animal in animals:
+        print(animal.make_sound())
+
